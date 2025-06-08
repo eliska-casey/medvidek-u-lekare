164d37487732513120a77806905036ec492df7ad
Eliška Casey <eliska.herudkova@gmail.com>
2025-06-08 14:42:27 +0200

    prvni commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+
+  // Zkopírování statických souborů do výstupní složky _site
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("fotky-na-web");
+  eleventyConfig.addPassthroughCopy("fotky-na-web-214x285px");
+  eleventyConfig.addPassthroughCopy("icons");
+  eleventyConfig.addPassthroughCopy("css"); // <- důležité!
+  // eleventyConfig.addPassthroughCopy("style.css"); // Nepotřebuješ, pokud styl je v css/style.css
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    },
+    templateFormats: ["njk", "html", "md", "liquid"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+
+};
